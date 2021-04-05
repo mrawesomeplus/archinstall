7307ac55bd42d1e959161edfabecaaf9cc125ba9
MrAwesomeplus <75816844+mrawesomeplus@users.noreply.github.com>
2021-04-05 12:49:19 -0500

    Fixed some issues

--- base.sh
+++ base.sh
@@ -11,9 +11,7 @@ echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 echo -n "Enter hostname : "
 read HOSTNAME
 echo $HOSTNAME >> /etc/hostname
-echo "127.0.0.1     localhost"
-echo "::1           localhost"
-echo "127.0.1.1     $HOSTNAME.localdomain $HOSTNAME"
+echo "127.0.0.1 localhost"\n"::1 localhost"\n"127.0.1.1 $HOSTNAME.localdomain $HOSTNAME" >> /etc/hosts
 
 # Root passwd
 passwd
@@ -23,7 +21,7 @@ sed -i '92s/.//' /etc/pacman.conf
 sed -i '93s/.//' /etc/pacman.conf
 
 # Install packages
-pacman -S --noconfirm grub efibootmgr networkmanager network-manager-applet reflector base-devel linux-headers bluez bluez-utils cups pulseaudio bash-completion openssh reflector virt-manager qemu edk2-ovmf bridge-utils dnsmasq ebtables libvirt os-prober dhcpcd
+pacman -Sy --noconfirm grub efibootmgr networkmanager network-manager-applet reflector base-devel linux-headers bluez bluez-utils cups pulseaudio bash-completion openssh reflector virt-manager qemu edk2-ovmf bridge-utils dnsmasq ebtables libvirt os-prober dhcpcd
 
 # Install grub
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
@@ -32,7 +30,7 @@ grub-mkconfig -o /boot/grub/grub.cfg
 # user creation
 echo -n "Name : "
 read name
-useradd -mG wheel $name
+useradd -mG wheel -s /bin/bash $name
 passwd $name
 
 # Enable services
