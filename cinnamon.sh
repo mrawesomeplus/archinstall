668907501201a16645feaa14fc4eb7f214b70e40
MrAwesomeplus <75816844+mrawesomeplus@users.noreply.github.com>
2021-04-06 16:18:01 -0500

    Update cinnamon.sh

--- cinnamon.sh
+++ cinnamon.sh
@@ -3,13 +3,9 @@
 su
 
 # Install packages
-sudo pacman -Sy lightdm cinnamon gedit gdisk mpv nemo gnome-terminal
+sudo pacman -Sy cinnamon lightdm-gtk-greeter lightdm-gtk-greeter-settings gedit gdisk mpv nemo gnome-terminal 
 sudo pacman -R xf86-video-intel
 
-# AUR Packages
-cp ./cinnamon/makepkg.conf /etc/makepkg
-yay -Sy lightdm-slick-greeter mint-themes-git xcursor-dmz pamac-all-git appimagelauncher lightdm-slick-greeter
-cp ./cinnamon/lightdm.conf /etc/lightdm/lightdm.conf
 
 sudo systemctl enable lightdm.service
 
