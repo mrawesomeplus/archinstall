b8ee0ff4abbd6c4566c17f28408f5692538f6580
MrAwesomeplus <75816844+mrawesomeplus@users.noreply.github.com>
2021-04-10 02:25:32 +0000

    Update cinnamon.sh

--- cinnamon.sh
+++ cinnamon.sh
@@ -5,6 +5,7 @@ su
 # Install packages
 sudo pacman -Sy cinnamon lightdm-gtk-greeter lightdm-gtk-greeter-settings gedit gdisk mpv nemo terminator blueberry gimp libre-office 
 sudo pacman -R xf86-video-intel
+yay -S mint-themes mint-y-icons cinnamon-sound-effects 
 
 
 sudo systemctl enable lightdm.service
