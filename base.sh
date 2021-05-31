0178bfa30f2304ab9777ca90ef6923ea5e23aa8c
MrAwesomeplus <75816844+mrawesomeplus@users.noreply.github.com>
2021-05-31 12:37:41 +0000

    Update base.sh

--- base.sh
+++ base.sh
@@ -21,11 +21,12 @@ sed -i '92s/.//' /etc/pacman.conf
 sed -i '93s/.//' /etc/pacman.conf
 
 # Install packages
-pacman -Sy grub grub-btrfs efibootmgr amd-ucode networkmanager network-manager-applet reflector base-devel linux-headers bluez bluez-utils cups pulseaudio bash-completion openssh reflector virt-manager qemu edk2-ovmf bridge-utils dnsmasq ebtables libvirt openbsd-netcat os-prober dhcpcd ntfs-3g xdg-user-dirs xdg-utils nfs-utils inetutils dnsutils firewalld
+pacman -Sy efibootmgr amd-ucode networkmanager network-manager-applet reflector base-devel linux-headers bluez bluez-utils cups pulseaudio bash-completion openssh reflector virt-manager qemu edk2-ovmf bridge-utils dnsmasq ebtables libvirt openbsd-netcat os-prober dhcpcd ntfs-3g xdg-user-dirs xdg-utils nfs-utils inetutils dnsutils firewalld
 
 # Install grub
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
-grub-mkconfig -o /boot/grub/grub.cfg
+# pacman -S grub grub-btrfs
+# grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+# grub-mkconfig -o /boot/grub/grub.cfg
 
 # user creation
 echo -n "Name : "
